5ecb84fee7c13b896e40ef2061b59895b96536e5
Michelle Burton <msmichelleburton@yahoo.com>
2025-12-22 17:24:07 -0600

    first commit

---
+++ awesome-websites/src/app/(experiments)/1-gsap-basics/page.tsx
@@ -0,0 +1,3 @@
+import Page from "./starter_my";
+
+export default Page;
---
+++ awesome-websites/src/app/(experiments)/1-gsap-basics/starter_my.tsx
@@ -0,0 +1,34 @@
+"use client";
+import gsap from "gsap";
+import { useEffect } from "react";
+// testing library
+// hello
+
+export default function Page() {
+
+    useEffect(() => {
+        gsap.fromTo(".title", {
+            x: -200,
+        }, {
+            x: 200,
+            onUpdate: () => {
+                console.log("ran");
+            }
+        });
+        return () => {
+
+        }
+    }, [])
+
+  return (
+    <div className="bg-blue-300 text-black">
+      <div className="flex h-screen items-end justify-left overflow-hidden">
+        <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
+          GSAP
+          <br />
+          tweens
+        </h1>
+      </div>
+    </div>
+  );
+}
